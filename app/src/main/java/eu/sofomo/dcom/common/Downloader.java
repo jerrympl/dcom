ea9c90197dbc4e20022b5046874b266c904e775b
bartomiej <bartomiej.buczek@sofomo.eu>
2015-07-10 10:41:35 +0200

    test2

--- app/src/main/java/eu/sofomo/dcom/common/Downloader.java
+++ app/src/main/java/eu/sofomo/dcom/common/Downloader.java
@@ -166,24 +166,6 @@ public class Downloader extends AsyncTask<Void, Void, String>
         return result;
     }
 
-    public static String sha256(String base) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(base.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer();
-
-            for (int i = 0; i < hash.length; i++) {
-                String hex = Integer.toHexString(0xff & hash[i]);
-                if(hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-
-            return hexString.toString();
-        } catch(Exception ex){
-            throw new RuntimeException(ex);
-        }
-    }
-
     @Override
     protected void onPreExecute()
     {
@@ -196,7 +178,7 @@ public class Downloader extends AsyncTask<Void, Void, String>
     {
         TextView out = (TextView) mActivity.findViewById(R.id.out);
         out.setText(result);
-        Log.e("qqqq", result);
+        Log.e("Resutl error: ", result);
 
         if(result != null) {
             ProgressBar progressBar = (ProgressBar) mActivity.findViewById(R.id.loading);
